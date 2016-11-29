8fc2c6243b83b1253c9278dbe757a0ee7e0c0117
rouz majlessi <rmajlessi@gmail.com>
2016-11-29 13:19:17 -0800

    create Controllers folder and AccountControllers file and set up user and sign in services

---
+++ src/BasicAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BasicAuthentication.Models;
+using Microsoft.AspNetCore.Identity;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BasicAuthentication.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+
+
+        // GET: /<controller>/
+        // This is were we set up the Identity or user and Sign in services/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
