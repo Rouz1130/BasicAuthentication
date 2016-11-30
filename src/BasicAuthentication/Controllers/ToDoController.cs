45f09ea6636ee3064299e83dcace146c6dbda7f0
rouz majlessi <rmajlessi@gmail.com>
2016-11-29 16:23:46 -0800

    add a todo controller file to manage to do lists

---
+++ src/BasicAuthentication/Controllers/ToDoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BasicAuthentication.Controllers
+{
+    // Authorize allows access to the controller only if user is logged in/
+    [Authorize]
+    public class ToDoController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        
+        public ToDoController (UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
