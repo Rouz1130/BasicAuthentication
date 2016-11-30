df1614083f3631b31048c54f665cd457cfe896a4
rouz majlessi <rmajlessi@gmail.com>
2016-11-29 16:43:34 -0800

    add to do list entries as a model for index add the acction to async in controller for todo

--- src/BasicAuthentication/Controllers/ToDoController.cs
+++ src/BasicAuthentication/Controllers/ToDoController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using BasicAuthentication.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,30 +18,31 @@ namespace BasicAuthentication.Controllers
     {
         private readonly ApplicationDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
-        
-        public ToDoController (UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+
+        public ToDoController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
         {
             _userManager = userManager;
             _db = db;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            return View(_db.Items.Where(x => x.User.Id == currentUser.Id));
+        }
+        public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Create(Item item)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            item.User = currentUser;
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-    public IActionResult Create()
-    {
-        return View();
-    }
-    [HttpPost]
-    public async Task<IActionResult> Create(Item item)
-    {
-        var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        var currentUser = await _userManager.FindByIdAsync(userId);
-        item.User = currentUser;
-        _db.Items.Add(item);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
-    }
-
 }
